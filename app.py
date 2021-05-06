9961852a7ce3b987e261243e8fe8ed65669259ed
mikespub <github@mikespub.net>
2021-05-07 01:14:05 +0200

    black format

--- app.py
+++ app.py
@@ -13,19 +13,30 @@ app = Flask(__name__)
 def hello():
     from flask import request
     import json
+
     info = json.dumps(request.environ, indent=2, default=lambda o: repr(o))
     try:
-        visits = redis.incr('counter')
+        visits = redis.incr("counter")
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    tmpl = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}<br/>" \
-           "<b>File:</b> {file}<br/>" \
-           "<b>Environ:</b> <pre>{environ}</pre>"
-    return tmpl.format(name=os.getenv('NAME', "world"), hostname=socket.gethostname(), visits=visits, file=__file__, environ=escape(info))
+    tmpl = (
+        "<h3>Hello {name}!</h3>"
+        "<b>Hostname:</b> {hostname}<br/>"
+        "<b>Visits:</b> {visits}<br/>"
+        "<b>File:</b> {file}<br/>"
+        "<b>Environ:</b> <pre>{environ}</pre>"
+    )
+    return tmpl.format(
+        name=os.getenv("NAME", "world"),
+        hostname=socket.gethostname(),
+        visits=visits,
+        file=__file__,
+        environ=escape(info),
+    )
 
 
 if __name__ == "__main__":
-	app.run(host=os.getenv('FLASK_HOST', '0.0.0.0'), port=int(os.getenv('FLASK_PORT', 5080)))
+    app.run(
+        host=os.getenv("FLASK_HOST", "0.0.0.0"), port=int(os.getenv("FLASK_PORT", 5080))
+    )
--- guniconfig.py
+++ guniconfig.py
@@ -1,4 +1,4 @@
 # gunicorn configuration file
 import os
 
-bind = "%s:%s" % (os.getenv('FLASK_HOST', '0.0.0.0'), os.getenv('FLASK_PORT', 5080))
+bind = "%s:%s" % (os.getenv("FLASK_HOST", "0.0.0.0"), os.getenv("FLASK_PORT", 5080))
