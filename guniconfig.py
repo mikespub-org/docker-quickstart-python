9fffc76e49324062444ea99d9b0ab0a90d8cba5d
mikespub <github@mikespub.net>
2021-05-28 19:06:33 +0200

    add gunicorn config

--- guniconfig.py
+++ guniconfig.py
@@ -2,3 +2,12 @@
 import os
 
 bind = "%s:%s" % (os.getenv("FLASK_HOST", "0.0.0.0"), os.getenv("FLASK_PORT", 5080))
+
+# Run the web service on container startup. Here we use the gunicorn
+# webserver, with one worker process and 8 threads.
+# For environments with multiple CPU cores, increase the number of workers
+# to be equal to the cores available.
+# workers = 1
+# threads = 8
+# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
+# timeout = 0
