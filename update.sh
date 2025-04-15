5c444accdf9bc2c8a2e17af9441c19c34c725596
mikespub <github@mikespub.net>
2025-04-15 19:18:36 +0200

    enable provenance and SBOM

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-docker build -t mikespub/quickstart-python:latest -f Dockerfile .
-docker build -t mikespub/quickstart-python:alpine -f Dockerfile.alpine .
+#
+# Enable provenance and SBOM - see https://earthly.dev/blog/containerd-docker/
+#
+docker build --provenance=true --sbom=true -t mikespub/quickstart-python:latest -f Dockerfile .
+docker build --provenance=true --sbom=true -t mikespub/quickstart-python:alpine -f Dockerfile.alpine .
 docker push mikespub/quickstart-python:latest
 docker push mikespub/quickstart-python:alpine
